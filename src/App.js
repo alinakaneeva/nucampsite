1e88838cbbda36954bdcf59f81f0ba51d8edea60
Alina Kaneeva <kaneeva.alina@gmail.com>
2025-02-19 13:04:30 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 // import { Container, Navbar, NavbarBrand } from 'reactstrap';
 // import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
+//import CampsitesList from './features/campsites/CampsitesList.js';
 // import { CAMPSITES } from './app/shared/CAMPSITES';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList campsite={[0]}/>
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );

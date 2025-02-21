7496f9c4bd526e3fe654136cbb99cb3489e76dfe
Alina Kaneeva <kaneeva.alina@gmail.com>
2025-02-21 14:02:24 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-// import React from 'react';
-// import { Container, Navbar, NavbarBrand } from 'reactstrap';
-// import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-//import CampsitesList from './features/campsites/CampsitesList.js';
-// import { CAMPSITES } from './app/shared/CAMPSITES';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
